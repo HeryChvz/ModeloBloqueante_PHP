89aae556947e71e00a5957b4285f76e214724f2f
Hery <reyheri2010@hotmail.com>
2021-11-07 22:51:46 -0700

    Cambio en nombre del archivo

--- modelobloqueantePHP.php
+++ modelobloqueantePHP.php
@@ -1,12 +1,12 @@
 <?php
     printf("Abrir  archivo");
-    $archivo_url = fopen("el_quijote.txt","r");
+    $archivo_url = fopen("quijote.txt","r");
     $texto = "";
     while ($fragmento = fgets($archivo_url)){
         $texto .= $fragmento;
     }   
     printf($texto);
-    printf("<br> hacer algo");
+    printf("<br> Hacer algo");
 ?>
 
 <script>
